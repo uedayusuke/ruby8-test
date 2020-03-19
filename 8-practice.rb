6a6b275f63e3f3dc2a84c7bc35bcc5c738da85bb
上田悠介 <uedayuusuke@uedayuusukenoMacBook-puro.local>
2020-03-19 15:27:02 +0900

    [Finish]2-8

---
+++ 8-practice.rb
@@ -0,0 +1,30 @@
+input_key = gets
+puts "入力された内容は" + input_key
+
+a = gets.to_i
+b = gets.to_i
+puts "a + b = #{a + b}"
+
+dice = 0
+
+while dice != 6 do
+	dice = rand(1..6)
+	puts dice
+end
+
+for i in 1..10 do
+	puts i
+end
+
+{"apple" => 130, "strawberry" => 180, "orange" => 100}.each do |fruit, price|
+	puts "#{fruit}は#{price}円です。"
+end
+
+i = 0
+while i <= 10 do
+	if i > 5
+		break
+	end
+	puts i
+	i += 1
+end
